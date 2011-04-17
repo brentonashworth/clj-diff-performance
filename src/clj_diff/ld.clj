58de0ff5c7651ff9f39eb9f924b7b6a8ed93a8c4
Brenton Ashworth <bashwort@mantooth.local>
2011-04-16 17:25:09 -0700

    Start thinking about fast ld.

--- src/clj_diff/ld.clj
+++ src/clj_diff/ld.clj
@@ -1,4 +1,4 @@
 (ns clj-diff.ld
   "Fast implementation of Levenshtein Distance."
-  (:require [clj-diff [optimizations :as opt]]))
-
+  (:require [clj-diff [optimizations :as opt]]
+            [clj-diff [miller :as m]]))
--- src/clj_diff/other_ld.clj
+++ src/clj_diff/other_ld.clj
@@ -1,6 +1,13 @@
 (ns clj-diff.other-ld
   "Other Levenshtein Disitance functions for use as a comparison.")
 
+;; A very compact implementation from Laurent Petit. This algorithm is also
+;; faster then the typical implementation. If you can make an algorithm
+;; faster than this, you are doing well.
+
+;; One thing you may want to investigate is using this algorithm with
+;; the data that you get from ses.
+
 (defn- new-row
   [prev-row row-elem t]
   (reduce (fn [row [d-1 d e]]
@@ -11,8 +18,7 @@
           [(inc (first prev-row))]
           (map vector prev-row (next prev-row) t)))
 
-(defn laurent-levenshtein
-  "Very compact implementation from Laurent PETIT."
+(defn laurent-levenshtein  
   [s t]
   (peek (reduce (fn [prev-row s-elem]
                   (new-row prev-row s-elem t))
